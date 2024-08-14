ec7efac370272b9eba5791091890979ec029cbfb
punchpatcha <patcharaluk.klin@gmail.com>
2024-08-14 23:42:13 +0700

    Update app.py to load dataset from GitHub

--- app.py
+++ app.py
@@ -17,8 +17,9 @@ CORS(app)  # Allow cross-origin requests
 
 # Function to load data from GitHub
 def load_data_from_github():
-    url = 'https://raw.githubusercontent.com/punchpatcha/thai-sms-detection/master/spamsmsdataset.csv?token=GHSAT0AAAAAACWAOMF5LRBHRRDSLNCLCEWSZV43QDA'
+    url = 'https://raw.githubusercontent.com/punchpatcha/thai-sms-detection/master/spamsmsdataset.csv?token=GHSAT0AAAAAACWAOMF4UEC5NKFFARWGPFJWZV43XPA'
     response = requests.get(url)
+    response.raise_for_status()  # Ensure we notice bad responses
     csv_data = StringIO(response.text)
     df = pd.read_csv(csv_data, encoding='utf-8')
     return df
@@ -26,9 +27,14 @@ def load_data_from_github():
 # Model training code 
 def train_model():
     sms = load_data_from_github()
-    sms.dropna(inplace=True, axis=1)
-    sms.columns = ["label", "msg"]
-    sms["label_sign"] = sms.label.map({"ham": 0, "spam": 1})
+    
+    # Print the first few rows to understand the structure
+    print("First few rows of the dataset:")
+    print(sms.head())
+
+    sms.columns = ["v1", "v2"] 
+    sms.dropna(inplace=True)
+    sms["label_sign"] = sms.v1.map({"ham": 0, "spam": 1})
 
     def clean_word(mess):
         nopunc = [char for char in mess if char not in string.punctuation and not any(emoji.is_emoji(c) for c in char)]
@@ -38,10 +44,10 @@ def train_model():
         filtered_sentence = [w for w in word_tokens if w not in stopwords_th]
         return ' '.join(filtered_sentence)
 
-    sms["clean_msg"] = sms.msg.apply(clean_word)
-    sms.dropna(subset=['label_sign'], inplace=True, axis=0)
+    sms["clean_msg"] = sms.v2.apply(clean_word)
+    sms.dropna(subset=['label_sign'], inplace=True)
 
-    X_train, X_test, y_train, y_test = train_test_split(sms.msg, sms.label_sign, random_state=1)
+    X_train, X_test, y_train, y_test = train_test_split(sms.clean_msg, sms.label_sign, random_state=1)
     vect = CountVectorizer()
     X_train_dim = vect.fit_transform(X_train)
     X_test_dim = vect.transform(X_test)
