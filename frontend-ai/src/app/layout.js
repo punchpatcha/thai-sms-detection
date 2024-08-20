b8f6ef89cede1a4a077a87658a6baff0f593db8c
punchpatcha <patcharaluk.klin@gmail.com>
2024-08-20 18:05:24 +0700

    Update favicon and title

--- frontend-ai/src/app/layout.js
+++ frontend-ai/src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/favicon.png" />
+        <link rel="icon" href="C:\Punch\Thai-sms-detection\frontend-ai\public\favicon.png" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
