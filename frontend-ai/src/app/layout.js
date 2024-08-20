4a164160bd59e9decfeda326a42f05076228d9d3
punchpatcha <patcharaluk.klin@gmail.com>
2024-08-20 18:29:00 +0700

    Added custom favicon and updated title

--- frontend-ai/src/app/layout.js
+++ frontend-ai/src/app/layout.js
@@ -12,7 +12,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="public/favicon.png" />
+        <link rel="icon" href="/favicon.png" />
       </head>
       <body className={inter.className}>{children}</body>
     </html>
