70888a68979522bce5922066dd2043189e5e8d17
punchpatcha <patcharaluk.klin@gmail.com>
2024-08-20 15:48:38 +0700

    Updating code

--- app.py
+++ app.py
@@ -26,30 +26,34 @@ def load_model_and_vectorizer():
 # Load the model and vectorizer
 clf, cv = load_model_and_vectorizer()
 
-# Function to clean and tokenize SMS messages
+# Function to clean text
 def clean_word(mess):
     nopunc = [char for char in mess if char not in string.punctuation and not any(emoji.is_emoji(c) for c in char)]
     nopunc = ''.join(nopunc)
     word_tokens = mmcut(nopunc)
     stopwords_th = thai_stopwords()
     filtered_sentence = [w for w in word_tokens if w not in stopwords_th]
-    return filtered_sentence
+    return ' '.join(filtered_sentence)
 
-# Preprocess data to find top spam words
-file_path ="C:\Punch\Thai-sms-detection\backend-ai\spamsmsdataset.csv"
-sms = pd.read_csv(file_path, encoding='utf-8')
-sms.dropna(inplace=True, axis=1)
-sms.columns = ["label", "msg"]
-sms["clean_msg"] = sms.msg.apply(clean_word)
+# Function to preprocess data and get top spam words
+def get_top_spam_words():
+    file_path = os.path.join(os.path.dirname(__file__), 'data', 'spamsmsdataset.csv')
+    sms = pd.read_csv(file_path, encoding='utf-8')
+    sms.dropna(inplace=True, axis=1)
+    sms.columns = ["label", "msg"]
+    sms["clean_msg"] = sms.msg.apply(clean_word)
 
-# Count words in spam messages
-spam_words_counter = Counter()
-for msg in sms[sms.label == 'spam']['msg']:
-    words = clean_word(msg)
-    spam_words_counter.update(words)
+    # Count words in spam messages
+    spam_words_counter = Counter()
+    stopwords_th = thai_stopwords()
+    for msg in sms[sms.label == 'spam']['clean_msg']:
+        words = mmcut(msg)
+        words = [word for word in words if re.match(r'^[ก-๙]+$', word) and word not in stopwords_th]
+        spam_words_counter.update(words)
 
-# Top 10 spam words
-top_spam_words = spam_words_counter.most_common(10)
+    # Get the top 10 most common words
+    top_spam_words = spam_words_counter.most_common(10)
+    return [{'word': word, 'frequency': count} for word, count in top_spam_words]
 
 @app.route('/predict', methods=['POST'])
 def predict():
@@ -60,10 +64,11 @@ def predict():
     return jsonify({"prediction": result})
 
 @app.route('/top-spam-words', methods=['GET'])
-def get_top_spam_words():
-    words_data = [{"word": word, "frequency": count} for word, count in top_spam_words]
-    return jsonify(words_data)
+def top_spam_words():
+    words = get_top_spam_words()
+    return jsonify(words)
 
 if __name__ == '__main__':
+    # Use the PORT environment variable provided by Render
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
