88b641a6d38ffae5764129ac3f55008f9e43fc64
Partcharaluk Klinsrisuk <patcharaluk.klin@gmail.com>
2024-08-29 14:47:01 +0700

    Update app.py

--- backend-ai/app.py
+++ backend-ai/app.py
@@ -1,23 +1,18 @@
 import os
-import re
-import string
-import emoji
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import pandas as pd
-import requests
-from io import StringIO
+import string
 from pythainlp.corpus import thai_stopwords
 from pythainlp.tokenize.multi_cut import mmcut
-from collections import Counter
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.svm import LinearSVC
 import pickle
+import emoji
 
 # Flask app setup
 app = Flask(__name__)
 CORS(app)  # Allow cross-origin requests
-
-# URL of the dataset on GitHub
-DATASET_URL = 'https://raw.githubusercontent.com/punchpatcha/thai-sms-detection/master/spamsmsdataset.csv'
 
 # Function to load the model and vectorizer
 def load_model_and_vectorizer():
@@ -37,39 +32,6 @@ def predict():
     prediction = clf.predict(vect_data)[0]
     result = "spam" if prediction == 1 else "ham"
     return jsonify({"prediction": result})
-
-@app.route('/top-spam-words', methods=['GET'])
-def top_spam_words():
-    # Fetch the CSV file from GitHub
-    url = 'https://raw.githubusercontent.com/punchpatcha/thai-sms-detection/master/spamsmsdataset.csv'
-    sms = pd.read_csv(url, encoding='utf-8')
-
-    # Process the data
-    sms.dropna(inplace=True, axis=1)
-    sms.columns = ["label", "msg"]
-    sms["label_sign"] = sms.label.map({"ham": 0, "spam": 1})
-
-    def clean_word(mess):
-        nopunc = [char for char in mess if char not in string.punctuation and not any(emoji.is_emoji(c) for c in char)]
-        nopunc = ''.join(nopunc)
-        word_tokens = mmcut(nopunc)
-        stopwords_th = thai_stopwords()
-        filtered_sentence = [w for w in word_tokens if w not in stopwords_th]
-        return ' '.join(filtered_sentence)
-
-    sms["clean_msg"] = sms.msg.apply(clean_word)
-    sms.dropna(subset=['label_sign'], inplace=True, axis=0)
-
-    # Count words
-    spam_words_counter = Counter()
-    for msg in sms[sms.label == 'spam']['clean_msg']:
-        words = mmcut(msg)
-        words = [word for word in words if re.match(r'^[ก-๙]+$', word)]
-        spam_words_counter.update(words)
-
-    top_spam_words = spam_words_counter.most_common(10)
-    return jsonify(top_spam_words)
-
 
 if __name__ == '__main__':
     # Use the PORT environment variable provided by Render
