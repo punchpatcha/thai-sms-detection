d40a3d78a839f994b4930c09ca5be080e8c6fe43
punchpatcha <patcharaluk.klin@gmail.com>
2024-08-30 22:08:26 +0700

    Updating code

--- app.py
+++ app.py
@@ -1,39 +1,36 @@
-import os
-from flask import Flask, request, jsonify
-from flask_cors import CORS
-import pandas as pd
 import string
-from pythainlp.corpus import thai_stopwords
-from pythainlp.tokenize.multi_cut import mmcut
-from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.svm import LinearSVC
+from flask import Flask, request, jsonify
 import pickle
+from pythainlp.tokenize.multi_cut import mmcut
+from pythainlp.corpus import thai_stopwords
+import re
 import emoji
 
-# Flask app setup
 app = Flask(__name__)
-CORS(app)  # Allow cross-origin requests
 
-# Function to load the model and vectorizer
-def load_model_and_vectorizer():
-    with open('model.pkl', 'rb') as f:
-        clf_svc = pickle.load(f)
-    with open('vectorizer.pkl', 'rb') as f:
-        vect = pickle.load(f)
-    return clf_svc, vect
+# Load the trained model and vectorizer
+clf_svc = pickle.load(open('spam_linear_svc_model.pkl', 'rb'))
+cv = pickle.load(open('transform.pkl', 'rb'))
 
-# Load the model and vectorizer
-clf, cv = load_model_and_vectorizer()
+def clean_word(mess):
+    nopunc = [char for char in mess if char not in string.punctuation and not any(emoji.is_emoji(c) for c in char)]
+    nopunc = ''.join(nopunc)
+    word_tokens = mmcut(nopunc)
+    stopwords_th = thai_stopwords()
+    filtered_sentence = [w for w in word_tokens if w not in stopwords_th]
+    return ' '.join(filtered_sentence)
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    data = request.json['message']
-    vect_data = cv.transform([data]).toarray()
-    prediction = clf.predict(vect_data)[0]
-    result = "spam" if prediction == 1 else "ham"
-    return jsonify({"prediction": result})
+    data = request.get_json()
+    message = data['message']
+    clean_message = clean_word(message)
+    vect_message = cv.transform([clean_message]).toarray()
+    prediction = clf_svc.predict(vect_message)[0]
+    
+    result = 'spam' if prediction == 1 else 'ham'
+    
+    return jsonify({'prediction': result})
 
-if __name__ == '__main__':
-    # Use the PORT environment variable provided by Render
-    port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+    app.run(debug=True)
